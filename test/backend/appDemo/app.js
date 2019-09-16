e6025454164eed8a085705535e9030139e4b6ace
Xiaoming Fu <xmingfu86@gmail.com>
2019-09-16 16:05:10 -0700

    restore file to frontend and backend directory

---
+++ test/backend/appDemo/app.js
@@ -0,0 +1,3 @@
+var cat = require("cat-me");
+
+console.log(cat());
---
+++ test/front_end/quiz12AgeCalc.js
@@ -0,0 +1,3 @@
+var age = prompt("What is your age?");
+var ageCalc = age * 365;
+alert(`${age} years is roughly ${ageCalc} days.`);
