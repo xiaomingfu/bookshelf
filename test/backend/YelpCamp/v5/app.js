b4d07ae6876bc7f9d98f2bd9ee4de0aec96f71e8
Xiaoming Fu <xmingfu86@gmail.com>
2019-09-23 14:36:38 -0700

    link login logout to navbar

--- test/backend/YelpCamp/v5/app.js
+++ test/backend/YelpCamp/v5/app.js
@@ -31,6 +31,10 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 // Campground.create(
 //     {
 //         name: "Grand Canyon",
