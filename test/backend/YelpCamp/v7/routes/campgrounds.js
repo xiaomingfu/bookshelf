b790c41e01e8eb65a213d4dd881678ba6b9bd488
Xiaoming Fu <xmingfu86@gmail.com>
2019-09-23 23:10:24 -0700

    add destroy route

--- test/backend/YelpCamp/v7/routes/campgrounds.js
+++ test/backend/YelpCamp/v7/routes/campgrounds.js
@@ -73,6 +73,16 @@ router.put("/:id", function(req, res){
         }
     });
 });
+//DESTROY ROUTE
+router.delete("/:id", function(req, res){
+    Campground.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/campgrounds");
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
+});
 
 //middleware
 function isLoggedIn(req, res, next){
